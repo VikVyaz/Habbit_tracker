cd97e0f922dbe461fe0b57b8472086960566cd1f
VikVyaz <gluckundhappy@gmail.com>
2025-11-18 20:10:25 +0300

    nginx + actions BRAND NEW)))) 5

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "drf-final"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
